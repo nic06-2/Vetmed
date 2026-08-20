63cf98e2dd69929115d3aca80fb92f40d1d38ed6
nic06-2 <nicolemedeiros669@gmail.com>
2026-08-20 18:58:36 -0300

    Create medicamentos.js with medication data
    Added a list of medications with their details.

---
+++ medicamentos.js
@@ -0,0 +1,73 @@
+const medicamentos = [
+  {
+    nome: "Dipirona",
+    principio: "A cadastrar",
+    classe: "Analgésico e antitérmico",
+    especies: ["Cães", "Gatos"],
+    indicacoes: "A cadastrar",
+    mecanismo: "A cadastrar",
+    dose: "A cadastrar",
+    via: "A cadastrar",
+    concentracao: "A cadastrar",
+    contraindicacoes: "A cadastrar",
+    efeitos: "A cadastrar",
+    interacoes: "A cadastrar",
+    toxicidade: "A cadastrar",
+    antidoto: "A cadastrar",
+    referencias: []
+  },
+
+  {
+    nome: "Prednisona",
+    principio: "A cadastrar",
+    classe: "Corticoide",
+    especies: ["Cães", "Gatos"],
+    indicacoes: "A cadastrar",
+    mecanismo: "A cadastrar",
+    dose: "A cadastrar",
+    via: "A cadastrar",
+    concentracao: "A cadastrar",
+    contraindicacoes: "A cadastrar",
+    efeitos: "A cadastrar",
+    interacoes: "A cadastrar",
+    toxicidade: "A cadastrar",
+    antidoto: "A cadastrar",
+    referencias: []
+  },
+
+  {
+    nome: "Amoxicilina",
+    principio: "A cadastrar",
+    classe: "Antimicrobiano",
+    especies: ["Cães", "Gatos"],
+    indicacoes: "A cadastrar",
+    mecanismo: "A cadastrar",
+    dose: "A cadastrar",
+    via: "A cadastrar",
+    concentracao: "A cadastrar",
+    contraindicacoes: "A cadastrar",
+    efeitos: "A cadastrar",
+    interacoes: "A cadastrar",
+    toxicidade: "A cadastrar",
+    antidoto: "A cadastrar",
+    referencias: []
+  },
+
+  {
+    nome: "Meloxicam",
+    principio: "A cadastrar",
+    classe: "Anti-inflamatório",
+    especies: ["Cães", "Gatos"],
+    indicacoes: "A cadastrar",
+    mecanismo: "A cadastrar",
+    dose: "A cadastrar",
+    via: "A cadastrar",
+    concentracao: "A cadastrar",
+    contraindicacoes: "A cadastrar",
+    efeitos: "A cadastrar",
+    interacoes: "A cadastrar",
+    toxicidade: "A cadastrar",
+    antidoto: "A cadastrar",
+    referencias: []
+  }
+];
